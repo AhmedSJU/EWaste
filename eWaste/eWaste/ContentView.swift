330dc2c7ccef43949525bc7b6323c91e49339497
Sheikh Ahmed <sheikh.ahmed@McBk-0133.cust.communityfibre.co.uk>
2022-09-11 09:27:35 +0100

    initial commit

---
+++ eWaste/eWaste/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  eWaste
+//
+//  Created by Sheikh Ahmed on 11/09/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ eWaste/eWaste/eWasteApp.swift
@@ -0,0 +1,17 @@
+//
+//  eWasteApp.swift
+//  eWaste
+//
+//  Created by Sheikh Ahmed on 11/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct eWasteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
